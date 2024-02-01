6dfd98d8a9c0850e98876a50124c37b64e0f1342
ZERRAI Sami <sami.zerrai@gmail.com>
2024-02-01 19:29:26 +0100

    Add securityConfig file

---
+++ src/main/java/com/example/library/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        request ->
+                        request
+                                .requestMatchers("/**/authenticate", "/**/register",
+                                        "/api/access/**",
+                                        "h2-console/**",
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                ).build();
+    }
+
+}
